2ca819691f4d55caa3eb0bb0193489657850876e
Ivan Manolov <ivan.manolov@appfire.com>
2025-03-31 08:25:20 +0300

    Initial commit

---
+++ static/api-perf/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import AppForge from "./AppForge";
+// import AppConnect from "./AppConnect";
+
+import "@atlaskit/css-reset";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <AppForge />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
